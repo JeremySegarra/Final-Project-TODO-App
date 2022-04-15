fe3a310368f4dff0552af81220bac0b890c3e40c
JeremySegarra <93934254+JeremySegarra@users.noreply.github.com>
2022-04-15 17:08:13 +0000

    Deploying to node-deploy from @ JeremySegarra/Final-Project-TODO-App@f986c5c22a1ce141d9a9399dac97069ccf73a8d5 🚀

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // app.use("/", express.static(__dirname + "/public/"));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 
