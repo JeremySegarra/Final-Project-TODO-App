d562d813f140036dff36bea7a4e7314df26eb238
jeremy segarra <jeremysegarra92@gmail.com>
2022-05-05 16:01:42 -0400

    made change to myFetch

--- client/src/models/myFetch.ts
+++ client/src/models/myFetch.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/api/";
+const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3001/api/";
 
 export async function api(url: string) {
   return fetch(API_URL + url).then((res) => res.json());
